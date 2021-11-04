74457a72f455619d4c9d2b28e4de8a68f93b8a41
Yixuan Qiu <yqiu22@colby.edu>
2021-11-04 12:28:26 -0400

    initial commit

---
+++ src/app.py
@@ -0,0 +1,40 @@
+
+
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+visitors = []
+
+
+@app.route("/")
+def index():
+	user = {'username' : 'Naser' }
+	return render_template("base.html", 
+							title="home", 
+							user=user, 
+							visitors=visitors)
+
+
+@app.route("/about")
+def about():
+	return "About us: Naser and the cool kids from CS321"
+
+
+@app.route("/add", methods=["POST"])
+def add():
+	visitor = request.form.get("visitor")
+	visitors.append(visitor)
+	print(visitors[-1])
+
+	return redirect(url_for("index"))
+
+
+@app.route("/remove/<string:name>")
+def remove(name):
+	visitors.remove(name)
+	return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+	app.run(debug=True) 
