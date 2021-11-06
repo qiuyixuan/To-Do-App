344c75adbfa6fb52bf0cec89b2338bee8ff85990
Yixuan Qiu <yqiu22@colby.edu>
2021-11-06 14:01:20 -0400

    added data and time

--- src/app.py
+++ src/app.py
@@ -5,25 +5,25 @@ Yixuan Qiu & Luhang Sun
 '''
 
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 to_do_list = []
 
-
 @app.route("/")
 def index():
-	# user = {'username' : 'Naser' }
+	now = datetime.now()
+	date_time = now.strftime("%m/%d/%Y, %H:%M")
 	return render_template("base.html", 
-							title="home", 
-							# user=user, 
+							title="Home", 
+							date_time=date_time,
 							to_do_list=to_do_list)
-
 
 @app.route("/about")
 def about():
 	return "A to-do app"
-
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -33,12 +33,10 @@ def add():
 
 	return redirect(url_for("index"))
 
-
 @app.route("/remove/<string:name>")
 def remove(name):
 	to_do_list.remove(name)
 	return redirect(url_for("index"))
-
 
 if __name__ == "__main__":
 	app.run(debug=True) 
