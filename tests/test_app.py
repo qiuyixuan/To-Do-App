a16b4e253a7205548b601f672df3ed9933734b89
Yixuan Qiu <yqiu22@colby.edu>
2021-11-06 14:39:35 -0400

    update test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,17 +4,17 @@ Project 3
 Yixuan Qiu & Luhang Sun
 '''
 
-from app import *
+from ../src/app import *
 import requests
 
 
-website_path = "https://salty-river-43291.herokuapp.com/"
-#website_path = "http://127.0.0.1:5000/"
+# website_path = "https://salty-river-43291.herokuapp.com/"
+website_path = "http://127.0.0.1:5000/"
 
 def test_about():
 	
 	# simple way to test webpage (words for basic pages)
-	assert about() == "About us: Naser and the cool kids from CS321"
+	assert about() == "A to-do app"
 
 	# more intreesting way to test the the page works
 	client = app.test_client()
@@ -27,7 +27,7 @@ def test_add():
 	# creating a post request with data as if coming from form
 	client = app.test_client()
 	url = '/add'
-	data = {'visitor': 'New person'}
+	data = {'item': 'to do'}
 	response = client.post(url, data=data)
 	
 	# making sure we got redirected, and the request didn't fail
@@ -36,7 +36,7 @@ def test_add():
 	# making sure the home page now includes the added test data
 	response = client.get("/")
 	webpage_text = response.get_data()
-	assert b'New person' in response.data
+	assert b'to do' in response.data
 
 
 
