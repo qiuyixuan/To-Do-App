830409e4e990bdd3cddcde118a06eb43f5a3006a
Luhang Sun <luhangsun@outlook.com>
2021-11-06 20:13:23 -0400

    add priority, tags, time features

--- src/app.py
+++ src/app.py
@@ -7,10 +7,11 @@ Yixuan Qiu & Luhang Sun
 from flask import Flask, render_template, request, url_for, redirect
 # from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from item import *
 
 app = Flask(__name__)
 
-to_do_list = []
+to_do_list = [] 
 
 @app.route("/")
 def index():
@@ -27,16 +28,47 @@ def about():
 
 @app.route("/add", methods=["POST"])
 def add():
-	item = request.form.get("item")
-	to_do_list.append(item)
-	print(to_do_list[-1])
+	content = request.form.get("item")
+	priority = request.form.get("priority")
+	tags_input = request.form.get("tags")	
+
+	#time
+	now = datetime.now()
+	time = now.strftime("%m/%d/%Y, %H:%M")
+
+	# create item dictionary with info above
+	item = {}
+	item["content"] = content
+	item["priority"] = priority
+	item["tags"] = formatTags(tags_input)
+	item["time"] = time
+
+	to_do_list.append(item) 
+	# print(to_do_list[-1])
 
 	return redirect(url_for("index"))
 
 @app.route("/remove/<string:name>")
 def remove(name):
-	to_do_list.remove(name)
+	# to_do_list.remove(name)
+	# newlist = [i for i in to_do_list if not (i["content"] == name)]
+	# to_do_list = newlist
+	for i in to_do_list:
+		if i["content"] == name:
+			to_do_list.remove(i)
+			break
 	return redirect(url_for("index"))
+
+
+# join tag list to a string for display
+def formatTags(tags_input):
+	if tags_input == "":
+		tags = ""
+	else:	
+		tag_list = tags_input.split(", ")
+		tags = "#" + " #".join(tag_list)
+	return tags
+
 
 if __name__ == "__main__":
 	app.run(debug=True) 
---
+++ src/item.py
@@ -0,0 +1,10 @@
+class Item:
+    def __init__(self, content, time, priority, tags=[]):
+        self.content = content
+        self.priority = priority
+        self.tags = tags
+        self.time = time
+
+    def setContent(content):
+        self.content = content
+    
