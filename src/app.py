8f9445a6b1cf7326b278a44a4d48fc91d5cda5c8
Yixuan Qiu <yqiu22@colby.edu>
2021-11-07 13:03:50 -0500

    Reformat app and test_app using Black

--- src/app.py
+++ src/app.py
@@ -1,71 +1,79 @@
-'''
+"""
 CS321
 Project 3
 Yixuan Qiu & Luhang Sun
-'''
+"""
 
 from flask import Flask, render_template, request, url_for, redirect
+
 # from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-to_do_list = [] 
+to_do_list = []
 content_list = []
+
 
 @app.route("/")
 def index():
-	now = datetime.now()
-	date_time = now.strftime("%m/%d/%Y, %H:%M")
-	return render_template("base.html", 
-							title="Home", 
-							date_time=date_time,
-							to_do_list=to_do_list,
-							content_list=content_list)
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M")
+    return render_template(
+        "base.html",
+        title="Home",
+        date_time=date_time,
+        to_do_list=to_do_list,
+        content_list=content_list,
+    )
+
 
 @app.route("/about")
 def about():
-	return "A to-do app"
+    return "A to-do app"
+
 
 @app.route("/add", methods=["POST"])
 def add():
-	content = request.form.get("item")
-	priority = request.form.get("priority")
-	tags_input = request.form.get("tags")	
+    content = request.form.get("item")
+    priority = request.form.get("priority")
+    tags_input = request.form.get("tags")
 
-	now = datetime.now()
-	time = now.strftime("%m/%d/%Y, %H:%M")
+    now = datetime.now()
+    time = now.strftime("%m/%d/%Y, %H:%M")
 
-	# create item dictionary with info above
-	item = {}
-	item["content"] = content
-	item["priority"] = priority
-	item["tags"] = formatTags(tags_input)
-	item["time"] = time
-	item["id"] = content + str(content_list.count(content))
+    # create item dictionary with info above
+    item = {}
+    item["content"] = content
+    item["priority"] = priority
+    item["tags"] = formatTags(tags_input)
+    item["time"] = time
+    item["id"] = content + str(content_list.count(content))
 
-	content_list.append(content)
-	to_do_list.append(item) 
+    content_list.append(content)
+    to_do_list.append(item)
 
-	return redirect(url_for("index"))
+    return redirect(url_for("index"))
+
 
 @app.route("/remove/<string:item_id>")
 def remove(item_id):
-	for item in to_do_list:
-		if item["id"] == item_id:
-			to_do_list.remove(item)
-			break
-	return redirect(url_for("index"))
+    for item in to_do_list:
+        if item["id"] == item_id:
+            to_do_list.remove(item)
+            break
+    return redirect(url_for("index"))
+
 
 # join tag list to a string for display
 def formatTags(tags_input):
-	if tags_input == "":
-		tags = ""
-	else:	
-		tag_list = tags_input.split(", ")
-		tags = "#" + " #".join(tag_list)
-	return tags
+    if tags_input == "":
+        tags = ""
+    else:
+        tag_list = tags_input.split(", ")
+        tags = "#" + " #".join(tag_list)
+    return tags
 
 
 if __name__ == "__main__":
-	app.run(debug=True) 
+    app.run(debug=True)
--- src/test_app.py
+++ src/test_app.py
@@ -1,8 +1,8 @@
-'''
+"""
 CS321
 Project 3
 Yixuan Qiu & Luhang Sun
-'''
+"""
 
 from app import *
 import requests
@@ -11,47 +11,51 @@ import requests
 # website_path = "https://salty-river-43291.herokuapp.com/"
 website_path = "http://127.0.0.1:5000/"
 
-def test_about():
-	
-	# simple way to test webpage (words for basic pages)
-	assert about() == "A to-do app"
 
-	# more intreesting way to test the the page works
-	client = app.test_client()
-	response = client.get("/about")
-	assert response.status_code == 200  # success
+def test_about():
+
+    # simple way to test webpage (words for basic pages)
+    assert about() == "A to-do app"
+
+    # more intreesting way to test the the page works
+    client = app.test_client()
+    response = client.get("/about")
+    assert response.status_code == 200  # success
 
 
 def test_add():
 
-	# creating a post request with data as if coming from form
-	client = app.test_client()
-	url = '/add'
-	data = {'item': 'to do'}
-	response = client.post(url, data=data)
-	
-	# making sure we got redirected, and the request didn't fail
-	assert response.status_code == 302  # redirect
+    # creating a post request with data as if coming from form
+    client = app.test_client()
+    url = "/add"
+    data = {"item": "to do"}
+    response = client.post(url, data=data)
 
-	# making sure the home page now includes the added test data
-	response = client.get("/")
-	webpage_text = response.get_data()
-	assert b'to do' in response.data
+    # making sure we got redirected, and the request didn't fail
+    assert response.status_code == 302  # redirect
+
+    # making sure the home page now includes the added test data
+    response = client.get("/")
+    webpage_text = response.get_data()
+    assert b"to do" in response.data
 
 
 def test_remove():
-	client = app.test_client()
-	url = "/remove"
-	data = {'item': 'to do'}
-	response = client.get(url, data=data)
+    client = app.test_client()
+    url = "/remove"
+    data = {"item": "to do"}
+    response = client.get(url, data=data)
 
-	# get redirected
-	assert response.status_code == 404 # redirect because the page does not exist anymore after the removal
+    # get redirected
+    assert (
+        response.status_code == 404
+    )  # redirect because the page does not exist anymore after the removal
 
-	# making sure the home page does not include the removed test data
-	response = client.get("/")
-	webpage_text = response.get_data()
-	assert b'to do' not in response.data
+    # making sure the home page does not include the removed test data
+    response = client.get("/")
+    webpage_text = response.get_data()
+    assert b"to do" not in response.data
+
 
 # url = website_path + '/add'
 # myobj = {'visitor': 'New person'}
