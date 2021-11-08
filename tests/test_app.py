3623f23b85a72511f24202e3621cf59ac488fe0f
Yixuan Qiu <yqiu22@colby.edu>
2021-11-08 11:52:24 -0500

    Add test_index

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,8 +10,22 @@ from app import *
 import requests
 
 
-# website_path = "https://salty-river-43291.herokuapp.com/"
-website_path = "http://127.0.0.1:5000/"
+website_path = "https://glacial-plains-37331.herokuapp.com/"
+# website_path = "http://127.0.0.1:5000/"
+
+def test_index():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200  # success
+
+    webpage_text = response.get_data()
+    assert b"A Minimalist To-Do App" in response.data
+    assert b"Current Date and Time is" in response.data
+    assert b"To-Do Item" in response.data
+    assert b"Due Date" in response.data
+    assert b"Priority" in response.data
+    assert b"Tags" in response.data
+    assert b"Add" in response.data
 
 
 def test_about():
@@ -19,7 +33,7 @@ def test_about():
     # simple way to test webpage (words for basic pages)
     assert about() == "A to-do app"
 
-    # more intreesting way to test the the page works
+    # more interesting way to test the the page works
     client = app.test_client()
     response = client.get("/about")
     assert response.status_code == 200  # success
