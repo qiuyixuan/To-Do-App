328d5b0599738681bf4879b49b03211e449bbdbf
Yixuan Qiu <yqiu22@colby.edu>
2021-11-09 22:29:17 -0500

    Reformat app_db.py

--- src/app_db.py
+++ src/app_db.py
@@ -10,29 +10,32 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     content = db.Column(db.String(200), nullable=False)
 
-@app.route('/', methods=['POST','GET'])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         return add()
     else:
         items = db.session.query(Todo).all()
         return render_template("index.html", title="Home", items=items)
 
+
 def add():
-    item = Todo(content=request.form['content'])
+    item = Todo(content=request.form["content"])
     db.session.add(item)
     db.session.commit()
     return redirect(url_for("index"))
 
 
-@app.route('/remove/<int:id>')
+@app.route("/remove/<int:id>")
 def remove(id):
     db.session.query(Todo).filter_by(id=id).delete()
     db.session.commit()
